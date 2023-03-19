41e756f043659c6de647854ac93e44b6a452e169
Aarão Melo Lopes <aarao.melo.lopes@gmail.com>
2023-03-19 13:00:55 -0400

    feat: Creation of user search and update services

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use DateTime;
 use DateTimeImmutable;
 use DateTimeZone;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,6 +23,16 @@ class UserController extends AbstractController
         ]);
     }
 
+    #[Route('users/{id}', name: 'FindUser', methods: ['GET'])]
+    public function find(int $id, UserRepository $userRepository): JsonResponse
+    {
+        $user = $userRepository->find($id);
+        if (!$user) throw $this->createNotFoundException();
+        return $this->json([
+            'data' => $user,
+        ]);
+    }
+
     #[Route('users', name: 'CreateUser', methods: ['POST'])]
     public function create(Request $request, UserRepository $userRepository): JsonResponse
     {
@@ -33,11 +44,31 @@ class UserController extends AbstractController
         $user->setCreatedAt(new DateTimeImmutable('now', new DateTimeZone('America/Sao_Paulo')));
         $user->setUpdatedAt(new DateTime('now', new DateTimeZone('America/Sao_Paulo')));
         $user->setIsAdmin($data['isAdmin']);
-
         $userRepository->save($user, true);
-
         return $this->json([
             'message' => 'User created successfully!',
+            'data' => $user,
+        ], 201);
+    }
+
+    #[Route('users/{id}', name: 'UpdateUser', methods: ['PUT', 'PATCH'])]
+    public function update(
+        int $id,
+        Request $request,
+        ManagerRegistry $doctrine,
+        UserRepository $userRepository
+    ): JsonResponse {
+        $data =  $request->request->all();
+        $user = $userRepository->find($id);
+        if (!$user) throw $this->createNotFoundException();
+        $user->setName($data['name']);
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+        $user->setUpdatedAt(new DateTime('now', new DateTimeZone('America/Sao_Paulo')));
+        $user->setIsAdmin($data['isAdmin']);
+        $doctrine->getManager()->flush();
+        return $this->json([
+            'message' => 'User updated successfully!',
             'data' => $user,
         ], 201);
     }
